e90ff255e163340dad8f12a5e0c5266bac2ba0dd
Stefan A <stadolf@gmail.com>
2018-09-17 21:51:38 +0200

    added simple api frontend

---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const {send} = require('micro')
+const microCors = require('micro-cors')
+const cors = microCors()
+
+const URL = require('url')
+const QS = require('querystring')
+const Cache = require("cache");
+
+const TwSearch = require('./twitter')({
+    CONSUMER_API_KEY: process.env.CONSUMER_API_KEY,
+    CONSUMER_API_SECRET: process.env.CONSUMER_API_SECRET
+});
+
+const CACHE_TTL = 3600 * 1000;
+
+const cache = new Cache(CACHE_TTL);
+
+const demoData = {
+    tags: [
+        'some',
+        'nice',
+        'hash',
+        'tags'
+    ]
+};
+
+module.exports = cors( (req , res) => {
+    const url = URL.parse(req.url);
+    const qs = QS.parse(url.query);
+    if (qs.demo == 'true') {
+        return send(res, 200, demoData);
+    }
+    if (!qs.tag || qs.tag.length < 2) {
+        return send(res, 500, 'pls give a tag');
+    }
+
+    let cached = cache.get(qs.tag)
+    if (cached) {
+        return send(res, 200, cached);
+    }
+
+    TwSearch(qs.tag, 5).then( tags => {
+        cache.put(qs.tag, {tags});
+        send(res, 200, {tags});
+    }).catch( (err, data) => {
+        send(res, 500, err);
+    });
+});
