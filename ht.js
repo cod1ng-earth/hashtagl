180325ebf6ad462cc25294e3707b99a7685540b1
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-09-02 20:34:49 +0200

    search followers logic

--- ht.js
+++ ht.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 
 const program = require('commander')
 const BestTags = require('./lib/bestTags')
+const Followers = require('./lib/followers')
 
 program.command('search <tag> [otherTags...]').action((tag, otherTags) => {
   const allTags = [tag].concat(otherTags)
@@ -18,8 +19,18 @@ program.command('search <tag> [otherTags...]').action((tag, otherTags) => {
     })
 })
 
-program.command('followers <user> [filter...]').action((user, filter) => {
-  console.log(user, filter)
+program.command('followers <user> <filter...>').action(async (user, filter) => {
+  // console.log(filter)
+  const followers = await Followers(user, filter.map(f => f.toLowerCase()))
+  console.dir(followers.map(f => ({
+    name: f.name,
+    screen_name: f.screen_name,
+    location: f.location,
+    description: f.description,
+    url: f.url,
+    twUrl: `https://twitter.com/${f.screen_name}`,
+    id: f.id_str
+  })))
 })
 
 program.parse(process.argv)
